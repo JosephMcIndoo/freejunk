284adb97375e91150595f9d3036d4bcded7bba2d
ThePyromancer23 <thepyromancer23@gmail.com>
2023-03-29 15:12:47 -0500

    Sidebar filter skeleton

---
+++ freejunk/src/components/Sidebar.js
@@ -0,0 +1,54 @@
+import {
+    Box,
+    Flex,
+    Text,
+    Checkbox,
+    VStack,
+    Button
+} from "@chakra-ui/react";
+
+
+// Temporary category list
+const Categories = [
+    {
+        name: 'School'
+    },
+    {
+        name: 'Electronics'
+    },
+    {
+        name: 'Toys'
+    }
+];
+
+const Sidebar = () => {
+    return (
+        <Box
+        bg="white"
+        borderRight="2px"
+        borderRightColor="gray.300"
+        pos="fixed"
+        w="200px"
+        h="full">
+            <Flex h="30px" alignItems="center" mx="8">
+                <Text fontSize="3xl">
+                    Filter
+                </Text>
+            </Flex>
+            <VStack align="left">
+                {Categories.map((c) => {
+                    return (
+                    <Checkbox colorScheme='teal' size='sm'>
+                        {c.name}
+                    </Checkbox>
+                    )
+                })}
+            </VStack>
+            <Button colorScheme='teal'>
+                Update Filters
+            </Button>
+        </Box>
+    )
+}
+
+export default Sidebar;
--- freejunk/src/pages/Mainpage.js
+++ freejunk/src/pages/Mainpage.js
@@ -1,13 +1,23 @@
 import Itemcard from "../components/Itemcard.js";
 import Search from "../components/Search.js";
 import Listjunk from "../components/Listjunk.js";
+import Sidebar from "../components/Sidebar.js";
+import "./mainpage.css";
+import { Flex, Box } from "@chakra-ui/react";
 
 //Very high level function again, dive into each component to get an idea
 function Mainpage(props) {
   return (
     <div>
       <Search mb="10px" />
-      <Listjunk />
+      <Flex>
+        <Box>
+          <Sidebar />
+        </Box>
+        <Box>
+          <Listjunk />
+        </Box>
+      </Flex>
     </div>
   );
 }
